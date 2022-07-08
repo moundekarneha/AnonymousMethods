286da887f7b75a5345cb07d051ac444551be7a36
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 17:12:05 +0530

    Add project files.

---
+++ AnonymousMethods/AnonymousClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethods
+{
+
+    //define delegate
+    public delegate string GreetingDelegate(string str);
+    internal class AnonymousClass
+    {
+        public static string Greetings(string str)
+        {
+            return "Hello " + str + ", very good morning to you :-)";
+        }
+
+        static void Main(string[] args)
+        {
+            GreetingDelegate gd = new GreetingDelegate(Greetings);
+            string s = gd.Invoke("Neha");
+
+            Console.WriteLine(s+"\n\n\n");
+
+            //or anonymous delegate
+            GreetingDelegate gd1 = delegate (string name)
+            {
+                return "Hello " + name + ", very good morning to you :-)";
+            };
+            string s2 = gd1.Invoke("Pranita");
+            Console.WriteLine("anonymous delegate\n"+s2 + "\n\n\n");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
